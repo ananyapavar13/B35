f12151edefaf192b7508f5987bd77a5fe4cb8f64
91797 <91797@LAPTOP-I2OTCL33>
2022-12-14 19:27:45 +0530

    first commit

---
+++ Day-16_StreamAPI/src/org/tnsindia/streamapidemo/MappingDemo.java
@@ -0,0 +1,15 @@
+package org.tnsindia.streamapidemo;
+
+import java.util.Arrays;
+import java.util.List;
+
+//Example on Map()operation
+public class MappingDemo {
+	
+	public static void main(String[] args) {
+		List<String>obj1=Arrays.asList(new String[] {"Ananya","Harshitha","Lavanya"});
+		obj1.stream().map(i->i.length()).forEach((i)->System.out.print(i+ " "));
+		
+	}
+
+}
