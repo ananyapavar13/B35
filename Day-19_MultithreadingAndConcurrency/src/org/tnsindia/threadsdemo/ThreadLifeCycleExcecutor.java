fb001495f68cbf3c85e1c92f72354dd58a031343
91797 <91797@LAPTOP-I2OTCL33>
2022-12-20 06:18:11 +0530

    first commit

---
+++ Day-19_MultithreadingAndConcurrency/src/org/tnsindia/threadsdemo/ThreadLifeCycleExcecutor.java
@@ -0,0 +1,12 @@
+package org.tnsindia.threadsdemo;
+
+public class ThreadLifeCycleExcecutor {
+
+	public static void main(String[] args) {
+		ThreadLifeCycleDemo d=new ThreadLifeCycleDemo();
+		d.start();
+		
+
+	}
+
+}
